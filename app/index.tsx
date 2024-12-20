a13c4bf3ddbe48c31d9ef803f9bab0d2274e6614
Nsplapp <nsplappdev@gmail.com>
2024-12-20 22:04:11 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>hello i am navneet .</Text>
+      <Text>where are we now</Text>
+    </View>
+  );
+}
